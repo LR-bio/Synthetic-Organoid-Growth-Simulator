fd764f9f3091621643b39ac2ee060daafbb4fdc9
LR <124655174+LR-bio@users.noreply.github.com>
2025-05-23 22:13:12 -0700

    Create half_adder.py

---
+++ examples/half_adder.py
@@ -0,0 +1,19 @@
+# examples/half_adder.py
+from core.logic_parser import LogicCircuit
+from core.visualize import draw_logic_circuit
+
+circuit = LogicCircuit()
+a = "A"
+b = "B"
+sum_out = circuit.add_gate("XOR", [a, b])
+carry_out = circuit.add_gate("AND", [a, b])
+
+bio_parts = circuit.generate_genetic_circuit()
+print("Generated Genetic Circuit:")
+for part in bio_parts:
+    print(part)
+
+print("\nExport to GenBank:")
+print(circuit.export_to_genbank())
+
+draw_logic_circuit(circuit)
